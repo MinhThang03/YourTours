5019905f9ed2e96139b73cc98e1738e44fb71ff1
MinhThang03 <minhhoangthang75@gmail.com>
2022-09-02 17:00:32 +0700

    init project

---
+++ src/main/java/com/hcmute/yourtours/YourToursApplication.java
@@ -0,0 +1,13 @@
+package com.hcmute.yourtours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourToursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourToursApplication.class, args);
+    }
+
+}
