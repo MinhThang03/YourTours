1c28d52fb5fa07cf3d67801b1ab522890854acd1
MinhThang03 <minhhoangthang75@gmail.com>
2022-10-06 22:34:08 +0700

    feat: auth controller

--- src/main/java/com/hcmute/yourtours/controllers/auth/IAuthController.java
+++ src/main/java/com/hcmute/yourtours/controllers/auth/IAuthController.java
@@ -3,13 +3,18 @@ package com.hcmute.yourtours.controllers.auth;
 
 import com.hcmute.yourtours.libs.model.factory.response.BaseResponse;
 import com.hcmute.yourtours.models.requests.LoginRequest;
+import com.hcmute.yourtours.models.requests.RefreshTokenRequest;
 import com.hcmute.yourtours.models.response.LoginResponse;
+import com.hcmute.yourtours.models.response.LogoutResponse;
+import com.hcmute.yourtours.models.response.RefreshTokenResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 public interface IAuthController {
@@ -17,5 +22,14 @@ public interface IAuthController {
     @Operation(summary = "Đăng nhập")
     @SecurityRequirements
     ResponseEntity<BaseResponse<LoginResponse>> login(@Valid @RequestBody LoginRequest request);
+
+    @DeleteMapping("/logout")
+    @Operation(summary = "Đăng xuất")
+    ResponseEntity<BaseResponse<LogoutResponse>> logout();
+
+    @PostMapping("/refresh-token")
+    @Operation(summary = "Refresh token")
+    ResponseEntity<BaseResponse<RefreshTokenResponse>> refreshToken(@Valid @RequestBody RefreshTokenRequest request, HttpServletRequest context);
+
 
 }
