64be7ad06c7c95698d8771d5f2c2fbed6dbff039
MinhThang03 <minhhoangthang75@gmail.com>
2022-11-20 17:25:44 +0700

    fix: date time format

--- src/main/java/com/hcmute/yourtours/models/price_of_home/request/GetPriceOfHomeRequest.java
+++ src/main/java/com/hcmute/yourtours/models/price_of_home/request/GetPriceOfHomeRequest.java
@@ -1,6 +1,7 @@
 package com.hcmute.yourtours.models.price_of_home.request;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
@@ -13,8 +14,10 @@ public class GetPriceOfHomeRequest {
     private UUID homeId;
 
     @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateFrom;
 
     @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateTo;
 }
