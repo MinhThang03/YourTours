9f420f6678f9d4db559df33e06fac6fe10e17d77
MinhThang03 <minhhoangthang75@gmail.com>
2023-05-01 18:42:54 +0700

    fix create comment request

--- src/main/java/com/hcmute/yourtours/models/booking/request/CreateCommentRequest.java
+++ src/main/java/com/hcmute/yourtours/models/booking/request/CreateCommentRequest.java
@@ -1,13 +1,11 @@
 package com.hcmute.yourtours.models.booking.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
 public class CreateCommentRequest {
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private UUID bookId;
     private Double rates;
     private String comment;
