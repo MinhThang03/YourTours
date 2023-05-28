a103c788d3fab459e4f21757a13d33be1b70b09a
MinhThang03 <minhhoangthang75@gmail.com>
2023-05-28 22:04:28 +0700

    setting error message with language

--- src/main/java/com/hcmute/yourtours/libs/factory/DefaultResponseFactory.java
+++ src/main/java/com/hcmute/yourtours/libs/factory/DefaultResponseFactory.java
@@ -1,15 +1,22 @@
 package com.hcmute.yourtours.libs.factory;
 
+import com.hcmute.yourtours.enums.LanguageEnum;
 import com.hcmute.yourtours.libs.exceptions.CustomException;
 import com.hcmute.yourtours.libs.exceptions.ErrorResponse;
 import com.hcmute.yourtours.libs.exceptions.IErrorCode;
 import com.hcmute.yourtours.libs.model.factory.response.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
+@Slf4j
 public class DefaultResponseFactory implements IResponseFactory {
 
     @Override
@@ -54,6 +61,23 @@ public class DefaultResponseFactory implements IResponseFactory {
         response.setCode(exception.getErrorCode().getCode());
         response.setSuccess(false);
         response.setMessage(getMessage(exception));
+
+        try {
+            RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+            if (requestAttributes instanceof ServletRequestAttributes) {
+                HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+                String language = request.getHeader("Accept-Language");
+                if (language.equalsIgnoreCase(LanguageEnum.EN.name())) {
+                    String errorMessage = exception.getErrorCode().name().replace("_", " ");
+                    response.setMessage(errorMessage);
+
+                }
+            }
+
+        } catch (Exception e) {
+            log.error("CONVERT MESSAGE ERROR LANGUAGE FAIL: {}", e.getMessage());
+        }
+
         return ResponseEntity
                 .status(exception.getErrorCode().getHttpStatus())
                 .contentType(MediaType.APPLICATION_JSON)
