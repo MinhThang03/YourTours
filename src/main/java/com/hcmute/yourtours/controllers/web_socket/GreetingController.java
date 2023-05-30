e3b9e50fa2806814084e8e36caf1830b020bc2f8
MinhThang03 <minhhoangthang75@gmail.com>
2023-05-30 22:40:01 +0700

    update config securities

--- src/main/java/com/hcmute/yourtours/controllers/web_socket/GreetingController.java
+++ src/main/java/com/hcmute/yourtours/controllers/web_socket/GreetingController.java
@@ -1,16 +1,20 @@
 package com.hcmute.yourtours.controllers.web_socket;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
+@Slf4j
 public class GreetingController {
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greet(HelloMessage message) {
+        log.info("Nhan tin tu socket: {}", message);
+
         return new Greeting("Hello, " +
                 HtmlUtils.htmlEscape(message.getName()));
     }
